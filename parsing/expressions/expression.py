fecca6ee552d77d2b0424508b8ce5ad4161c85f3
david <davidjmacdonald11@gmail.com>
2023-05-19 12:55:55 -0400

    Reset for new kotlin series

--- parsing/expressions/expression.py
+++
@@ -1,7 +0,0 @@
-from ..node import Node
-from .additive_expression import AdditiveExpression
-
-class Expression(Node):
-    @classmethod
-    def construct(cls, parser) -> Node:
-        return AdditiveExpression.construct(parser)
--- util/error.py
+++
@@ -1,6 +0,0 @@
-class LanguageError(RuntimeError):
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return f"{type(self).__name__}: {self.message}"
